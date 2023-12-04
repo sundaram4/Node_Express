552ea38c4b144130ab0f10764e9b93fb9939e967
Sundaram4 <kunalsundaram4@gmail.com>
2023-12-04 08:08:15 +0530

    Creating Controller Methods

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,35 +1,52 @@
 const express = require("express");
+const {
+  getBootcamp,
+  getBootcamps,
+  deleteBootcamp,
+  updateBootcamp,
+  createBootcamp,
+} = require("../controllers/bootcamp");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  //console.log(res)
-  // res.send('<h1>Hello World</h1>');
-  //res.json({name:'Brad'});
-  //send only status 200, 400, 300
-  //res.sendStatus(400);
-  //res.status(400).json({success:false});
-  //successful response
-  res
-    .status(200)
-    .json({ success: true, data: { id: 1, msg: "show all bootcamps" } });
-});
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `show bootcamp ${req.params.id}` });
-});
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: `Create new bootcamp` });
-});
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router
+  .route("/")
+  .get(getBootcamps)
+  .post(createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+// router.get("/", (req, res) => {
+//   //console.log(res)
+//   // res.send('<h1>Hello World</h1>');
+//   //res.json({name:'Brad'});
+//   //send only status 200, 400, 300
+//   //res.sendStatus(400);
+//   //res.status(400).json({success:false});
+//   //successful response
+//   res
+//     .status(200)
+//     .json({ success: true, data: { id: 1, msg: "show all bootcamps" } });
+// });
+// router.get("/:id", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ success: true, msg: `show bootcamp ${req.params.id}` });
+// });
+// router.post("/", (req, res) => {
+//   res.status(200).json({ success: true, msg: `Create new bootcamp` });
+// });
+// router.put("/:id", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+// });
+// router.delete("/:id", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+// });
 
 module.exports = router;
