55b651f2d8830c0109931fb087d63365f69add6a
Sundaram4 <kunalsundaram4@gmail.com>
2023-12-09 18:56:36 +0530

    Create Bootcamp Post Request

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ connectDB();
 
 const app = express();
 
+//Body Parser
+app.use(express.json())
+
 //Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
