f24fb49571c90db81945f361fc0c29756e849135
Sundaram4 <kunalsundaram4@gmail.com>
2023-12-12 07:50:21 +0530

    Mongoose Error Handling Part 1-- CastError

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -29,7 +29,7 @@ exports.getBootcamp = async (req, res, next) => {
         res.status(200).json({ success: true, data:bootcamp });
     }catch(err){
         //res.status(400).json({ success: false});
-        next(new ErrorResponse(`Bootcamp not found with ${req.params.id}`,404));
+        next(err);
     }
     //res.status(200).json({ success: true, msg: `show bootcamp ${req.params.id}` });
 }
@@ -62,12 +62,13 @@ exports.updateBootcamp = async(req, res, next) => {
         });
     
         if(!bootcamp){
-            res.status(400).json({ success: false});
+            return next(new ErrorResponse(`Bootcamp not found with ${req.params.id}`,404));
         }
         res.status(200).json({ success: true, data:bootcamp });
     }
     catch(err){
-        res.status(400).json({ success: false});
+        //res.status(400).json({ success: false});
+        next(err);
     }
 }
 
@@ -79,11 +80,12 @@ exports.deleteBootcamp = async (req, res, next) => {
     try{
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
         if(!bootcamp){
-            res.status(400).json({ success: false});
+            return next(new ErrorResponse(`Bootcamp not found with ${req.params.id}`,404));
         }
         res.status(200).json({ success: true, data:bootcamp });
 
     }catch(err){
-        res.status(400).json({ success: false});
+        //res.status(400).json({ success: false});
+        next(err);
     }
 }
--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,24 @@
+const errorResponse = require('../utills/errorResponse')
+
 const errorHandler = (err, req, res, next) => {
+  let error = {...err};
+
+  error.message = err.message;
+
   console.log(err.stack.red);
+  
+  //console log error name -- CastError || 
+  console.log(err.name);
+
+  // Mongoose bad ObjectId -- CastError
+  if(err.name === 'CastError'){
+    const message = `Resource not found with id ${err.value}`
+    error = new errorResponse(message, 404);
+  }
 
   res
-    .status(err.statusCode || 400)
-    .json({ success: false, message: err.message || "Server Error" });
+    .status(error.statusCode || 400)
+    .json({ success: false, message: error.message || "Server Error" });
 };
 
 module.exports = errorHandler;
