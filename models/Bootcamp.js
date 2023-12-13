7ba65ee93122fbc3594ac152f0ef08c5a6f1977a
Sundaram4 <kunalsundaram4@gmail.com>
2023-12-13 10:11:44 +0530

    Mongoose Middleware & slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require('slugify')
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -95,5 +96,11 @@ const BootcampSchema = new mongoose.Schema({
     default: Date.now,
   }
 });
+
+//Create bootcamp slug from the name
+BootcampSchema.pre("save", function(){
+  console.log(`logging name ${this.name}`);
+  this.slug = slugify(this.name,{lower:true});
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
