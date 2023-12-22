92e5d55d1a0ac99dc58e8941bf1e174ec7f0f6b8
Sundaram4 <kunalsundaram4@gmail.com>
2023-12-22 08:50:10 +0530

    User Model,Register_Encrypting Passwords

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const morgan = require("morgan")
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 //Load env vars from config file
 dotenv.config({ path: "./config/config.env" });
@@ -43,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth',auth)
 
 //can use only after mounting routers
 // errorhandler
