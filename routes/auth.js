2b189804285d0d234c31c52c15d27777f1ba2644
Sundaram4 <kunalsundaram4@gmail.com>
2024-01-03 12:24:39 +0530

    Update User Details

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const{register,login, getMe,forgotPassword,resetPassword} = require('../controllers/auth')
+const express = require("express");
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
+} = require("../controllers/auth");
 
 const router = express.Router();
-const {protect} = require('../middleware/auth');
+const { protect } = require("../middleware/auth");
 
-router.post('/register',register)
-router.post('/login',login)
-router.get('/me',protect,getMe)
-router.post('/forgotpassword',forgotPassword)
-router.put('/resetpassword:resettoken',resetPassword)
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword:resettoken", resetPassword);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 
 module.exports = router;
